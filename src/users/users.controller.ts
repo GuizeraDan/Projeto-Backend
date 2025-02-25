8688f4d81415f55437ee5d381324d693c3db2416
Guilherme <guigui.dantas@gmail.com>
2025-02-25 13:44:55 -0300

    Co-authored-by: Rafael Dantas <Danzokka@users.noreply.github.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Put,
+  Get,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/CreateUserDto";
 
-@Controller('users')
-export class UsersController {}
+@Controller("users")
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  //backend.com.br/users
+
+  @Get() async getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+
+  //backend.com.br/users/:id
+
+  @Get(":id") async getUser(@Param("id") id: string) {
+    return this.usersService.getUser(parseInt(id));
+  }
+
+  @Post() async createUser(@Body() CreateUserDto: CreateUserDto) {
+    return this.usersService.createUser(CreateUserDto);
+  }
+
+  @Put() async updateUser(@Body() CreateUserDto: CreateUserDto) {
+    return this.usersService.updateUser(CreateUserDto);
+  }
+
+  @Delete() async deleteUser(@Body() CreateUserDto: CreateUserDto) {
+    //Enviar apenas username a partir do create user dto
+
+    const username = CreateUserDto.username;
+    return this.usersService.deleteUser(username);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,17 +2,24 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma.service";
 import { User, Prisma } from "@prisma/client";
 import bcrypt from "bcrypt";
+import { CreateUserDto } from './dto/CreateUserDto'
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
-  async createUser(data: {
-    username: string;
-    password: string;
-  }): Promise<User> {
+
+  async createUser(data: CreateUserDto): Promise<User> {
     const senhaCriptografada = await bcrypt.hash(data.password, 10);
     return this.prisma.user.create({
       data: { username: data.username, password: senhaCriptografada },
     });
   }
+
+  async getAllUsers(){}
+  
+  async getUser(id: number) {}
+
+  async updateUser(data: CreateUserDto) {}
+
+  async deleteUser(username: string){}
 }
